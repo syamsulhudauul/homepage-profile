52505bf10c028ee271e60b7e8e1db71b695abd86
syamsulhudauul <syamsulhuda.uul@gmail.com>
2024-07-21 20:55:53 +0700

    update again

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
+  output: 'export',
   assetPrefix: isProd ? '/my-next-app/' : '',
   basePath: isProd ? '/my-next-app' : '',
 };
