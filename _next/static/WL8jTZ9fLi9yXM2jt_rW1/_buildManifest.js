a02b61809e0a355ab22a0e57ab34d189f65035df
syamsulhudauul <syamsulhuda.uul@gmail.com>
2024-07-21 20:55:40 +0700

    Updates

---
+++ _next/static/WL8jTZ9fLi9yXM2jt_rW1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/4f5e1b70e6a68d91.css","static/chunks/pages/index-93a4ab85fa107c35.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
